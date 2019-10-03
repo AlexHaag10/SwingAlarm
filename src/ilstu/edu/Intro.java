bcf3feafcbfa16c150e581f7b7585ebbcfc961a4
Alex <alexhaag10@gmail.com>
2019-10-03 15:14:21 -0500

    Created framework with JTabbedPanel

--- src/ilstu/edu/Intro.java
+++ src/ilstu/edu/Intro.java
@@ -2,12 +2,7 @@ package ilstu.edu;
 
 public class Intro {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Yooooooo");
-		
-		
-		
-	}
+// Link to pdf for project 1
+	//https://reggienet.illinoisstate.edu/access/content/attachment/f62589e4-7b2d-4b8d-94d7-8a35619c057b/Assignments/4dfe5814-a1a6-47f5-8bc3-d3e49815406f/IT%20226%20Project%20-%20Java%20Swing%20Alarms.pdf
 
 }
