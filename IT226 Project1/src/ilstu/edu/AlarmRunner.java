432450cedb1fb0a28ea199645c2f79688475bec2
Alex <alexhaag10@gmail.com>
2019-10-16 17:32:37 -0500

    Added a file reader and added Kevin's classes

---
+++ IT226 Project1/src/ilstu/edu/AlarmRunner.java
@@ -0,0 +1,42 @@
+package ilstu.edu;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AlarmRunner
+{
+	public static void main(String[]args)
+	{
+		
+	
+	int x = 1;
+	while (x!=0) {
+		Alarm alarm = new Alarm(2019, 3, 10, 11, 13, 40);
+		alarm.addAlarm(new Alarm(2019, 3, 10, 11, 13, 45));
+
+		
+		alarm.addAlarm(alarm);
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date));
+		
+		//System.out.println("\n\n" + Calendar.getInstance().get(Calendar.SECOND)+ "\n\n");
+		
+		try
+		{
+			Thread.sleep(1000);
+		}
+		catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		alarm.soundAlarm();
+	
+	}
+	}
+}
